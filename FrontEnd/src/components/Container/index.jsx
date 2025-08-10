3a50c9f45688ce34907cec8ff0b031c00561afb1
Shaurya <pandeyshaurya2004@gmail.com>
2025-08-11 01:11:48 +0530

    Fix: backend CORS + socket config for deployment

--- FrontEnd/src/components/Container/index.jsx
+++ FrontEnd/src/components/Container/index.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Board } from '../../Board'
 
 export const Container = () => {
+
   return (
+
     <div className='h-[100vh] w-100 bg-black flex flex-col justify-center items-center'>
-        <div>
-            <input type="color"></input>
-        </div>
-        <div>
-            <Board/>
-        </div>
+      <div>
+        <input type="color"></input>
+      </div>
+      <div>
+        <Board />
+      </div>
     </div>
   )
 }
--- FrontEnd/src/components/Forms/index.jsx
+++ FrontEnd/src/components/Forms/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-import {v4 as uuidv4} from 'uuid'
-import {useNavigate} from "react-router-dom"
+import { v4 as uuidv4 } from 'uuid'
+import { useNavigate } from "react-router-dom"
 
-const JoinRoom = ({setUser,socket}) => {
-    const [id,setId]=useState("")//room id
-    const [name,setName]=useState("")
+const JoinRoom = ({ setUser, socket }) => {
+    const [id, setId] = useState("")//room id
+    const [name, setName] = useState("")
 
     const navigate = useNavigate()
+
 
     // function handleJoinRoom(e){
     //     e.preventDefault()
@@ -19,25 +20,25 @@ const JoinRoom = ({setUser,socket}) => {
     //     //     presenter:false
     //     // }
     //     // setUser(userData)
-        // navigate(`/${id}`)
+    // navigate(`/${id}`)
     //     // socket.emit('user-joined',userData)
     //     // console.log(userData)
     // }
-    
-function handleClick(){
-    console.log("hi")
-}
-  return (
-    <div>
-        {/* <form> */}
+
+    function handleClick() {
+        console.log("hi")
+    }
+    return (
+        <div>
+            {/* <form> */}
             {/* <div> */}
-                {/* <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter your name"></input>
+            {/* <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter your name"></input>
                 <input id="input" value={id} onChange={(e)=>setId(e.target.value)} type="text" placeholder="Enter Room Code"></input>
             </div> */}
-        <button onClick={handleClick}>Join Room</button>
-        {/* </form> */}
-    </div>
-  )
+            <button onClick={handleClick}>Join Room</button>
+            {/* </form> */}
+        </div>
+    )
 }
 
 export default JoinRoom
